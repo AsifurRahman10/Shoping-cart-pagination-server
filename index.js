e333580cc37f5cd4b4a8ed7b9f7e0e7509620ead
AsifurRahman10 <1720105@iub.edu.bd>
2024-12-22 21:12:50 +0600

    server side final commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.y6uow.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,13 +28,31 @@ async function run() {
     const productCollection = client.db('emaJohnDB').collection('products');
 
     app.get('/products', async (req, res) => {
-      const result = await productCollection.find().toArray();
+      const page = parseInt(req.query.page);
+      const size = parseInt(req.query.size)
+      const result = await productCollection.find()
+        .skip(page * size)
+        .limit(size)
+        .toArray();
       res.send(result);
     })
 
     app.get('/productCount', async (req, res) => {
       const count = await productCollection.estimatedDocumentCount();
       res.send({ count });
+    })
+
+    app.post('/productByIds', async (req, res) => {
+      const data = req.body;
+      console.log(data);
+      const idsWithObjectID = data.map(item => new ObjectId(item));
+      const query = {
+        _id: {
+          $in: idsWithObjectID
+        }
+      }
+      const result = await productCollection.find(query).toArray();
+      res.send(result)
     })
 
     // Send a ping to confirm a successful connection
